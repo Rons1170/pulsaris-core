e8b00bad147bb9252a02f0b19d6df1a1b7d598b6
Rons1170 <rons1170@gmail.com>
2025-08-06 15:56:20 -0400

    Update deploy_devnet.ts
    added Public Key and ID

--- scripts/deploy_devnet.ts
+++ scripts/deploy_devnet.ts
@@ -5,7 +5,57 @@ const connection = new Connection("https://api.devnet.solana.com");
 const wallet = new Wallet(Keypair.generate());
 const provider = new AnchorProvider(connection, wallet, {});
 const program = new Program(idl, programID, provider);
+ts
+import * as anchor from "@coral-xyz/anchor";
+import { Connection, Keypair, PublicKey } from "@solana/web3.js";
+import idl from "../target/idl/pulsaris_core.json";
+
+const programID = new PublicKey("YOUR_PROGRAM_ID_HERE");
+const connection = new Connection("https://api.devnet.solana.com");
+const wallet = anchor.Wallet.local();
+const provider = new anchor.AnchorProvider(connection, wallet, {});
+anchor.setProvider(provider);
+
+const program = new anchor.Program(idl, programID, provider);
+
+async function deploy() {
+const mintKeypair = Keypair.generate();
+
+await program.methods
+.initializeMint()
+.accounts({
+mint: mintKeypair.publicKey,
+user: provider.wallet.publicKey,
+rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+systemProgram: anchor.web3.SystemProgram.programId,
+})
+.signers([mintKeypair])
+.rpc();
+
+console.log("Mint deployed:", mintKeypair.publicKey.toBase58());
+}
+
+deploy();
+
+ts
+import idl from "../target/idl/pulsaris_core.json";
+const programID = new PublicKey("YOUR_PROGRAM_ID_HERE");
 
 // TODO: Add mint deployment logic here
 console.log("Deploying PULSARIS mint contract to devnet...");
-```
+
+
+const mintKeypair = Keypair.generate();
+
+await program.methods
+.initializeMint()
+.accounts({
+mint: mintKeypair.publicKey,
+user: provider.wallet.publicKey,
+rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+systemProgram: anchor.web3.SystemProgram.programId,
+})
+.signers([mintKeypair])
+.rpc();
+
+console.log("Mint deployed:", mintKeypair.publicKey.toBase58());
